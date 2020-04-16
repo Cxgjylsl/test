349c1cb7622537c09fef95a7a91f745a81ba8717
cxgjylsl <cxgjylsl@outlook.com>
2020-04-16 19:26:42 +0800

    test change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("222ss22");
+        System.out.println("22asdasd2ss22");
     }
 }
